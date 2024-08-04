3f30d0eabe5c1afbe243935d2fdfb99e5e523e23
sasha-sham <sasha.sham.dubinkina@gmail.com>
2024-08-04 16:59:29 +0300

    Update localizer.js

--- js/localizer.js
+++ js/localizer.js
@@ -59,7 +59,7 @@ var dictionary = {
         "type_recomendation": "Recommendations for visiting my art studio:",
         "text_recomendation_1": "you have a specific problem that you want to solve, but you don’t understand how to do it yourself;",
         "text_recomendation_2": "the desire to understand yourself more deeply (lifestyle, values, desires, goals, etc.);",
-        "text_recomendation_3": "the problematic situation is repeated systematically (connection with your personality, temperament, behavior patterns, injuries, etc.);",
+        "text_recomendation_3": "the problematic situation is repeated systematically (connection with your personality, temperament, behavior patterns, traumas, scars, etc.);",
         "text_recomendation_4": "thought about developing your creative potential and spending creative time, etc.",
 
         "type_kids": "Child's personality",
