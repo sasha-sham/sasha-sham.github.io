0238a43436dccb13aa6b7e48247bcce2fed8e520
sasha-sham <sasha.sham.dubinkina@gmail.com>
2024-08-09 20:10:09 +0300

    Update localizer.js

--- js/localizer.js
+++ js/localizer.js
@@ -24,7 +24,7 @@ var dictionary = {
         "text_recomendation_1": "у Вас есть конкретная проблема, которую Вы хотите решить, но не понимаете, как это сделать самостоятельно;",
         "text_recomendation_2": "преобладает желание глубже разобраться в себе (стиль жизни, ценности, желания, цели и др.);",
         "text_recomendation_3": "проблемная ситуация повторяется систематически (связь с Вашей личностью, темпераментом, моделями поведения, травмами и др.);",
-        "text_recomendation_4": "задумались о развитии Вашего творческого потенциала и креативного времени препровождения и др.",
+        "text_recomendation_4": "задумались о развитии Вашего творческого потенциала и креативного времени препровождения и др.:",
 
         "type_kids": "Личность ребенка",
         "text_kids": "Минимизация трудностей поведения и коммуникации, повышение адаптивности в меняющемся мире, концентрация на чувствах, подтверждение индивидуальности",
@@ -68,7 +68,7 @@ var dictionary = {
         "text_recomendation_1": "you have a specific problem that you want to solve, but you don’t understand how to do it yourself;",
         "text_recomendation_2": "the desire to understand yourself more deeply (lifestyle, values, desires, goals, etc.);",
         "text_recomendation_3": "the problematic situation is repeated systematically (connection with your personality, temperament, behavior patterns, traumas, scars, etc.);",
-        "text_recomendation_4": "thought about developing your creative potential and spend time creatively, etc.",
+        "text_recomendation_4": "thought about developing your creative potential and spend time creatively, etc.:",
 
         "type_kids": "Child's personality",
         "text_kids": "Minimizing behavioral and communication difficulties, increasing adaptability in a changing world, focusing on feelings, confirming individuality",
